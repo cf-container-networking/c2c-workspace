b97fea95b87d21151e7aaa09a352f95ccf7356b4
David McClure <dmcclure@pivotal.io>
2017-01-31 15:23:20 -0800

    Start prompt on new line
    Behaves better when window is narrow

--- git-prompt-colors.sh
+++ git-prompt-colors.sh
@@ -9,7 +9,7 @@ override_git_prompt_colors() {
   CfTarget="\$(cf-target)"
   GoBoshTarget="\$(env | grep BOSH_DIR | cut -d '/' -f 7)"
   GIT_PROMPT_START_USER="\n${Red}r:${BoshTarget} | g:${GoBoshTarget} | ${CfTarget} (\h) ${ResetColor}\n${Yellow}${PathShort}${ResetColor}"
-  GIT_PROMPT_END_USER=" ${Cyan}${GIT_PAIR}${ResetColor} $ "
+  GIT_PROMPT_END_USER=" ${Cyan}${GIT_PAIR}${ResetColor}\n$ "
   GIT_PROMPT_END_ROOT="\n# "
 
   GIT_PROMPT_PREFIX="|"
