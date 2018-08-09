30f7cdc28edd72b7501a2afbebed630d247ff927
Christian Ang <cang@pivotal.io>
2018-08-09 10:56:36 -0700

    Add story function
    Signed-off-by: Slawek Ligus <sligus@pivotal.io>

--- shared.bash
+++ shared.bash
@@ -416,5 +416,17 @@ function windows_port_forward() {
   ssh -f -L 3389:$1:3389 -N -i ${BOSH_GW_PRIVATE_KEY} ${BOSH_GW_USER}@${BOSH_GW_HOST}
 }
 
+function story() {
+  if [ -n "$TRACKER_API_TOKEN" ]; then
+    STORY_TITLE=" $(curl -s -H "X-TrackerToken: $TRACKER_API_TOKEN" \
+      "https://www.pivotaltracker.com/services/v5/projects/$TRACKER_PROJECT/stories/${1/\#/}" \
+      | jq -r .name)"
+  else
+    STORY_TITLE=''
+  fi
+  printf "\n\n[$1]$STORY_TITLE" > ~/.git-tracker-story
+}
+
+
 main
 unset -f main
